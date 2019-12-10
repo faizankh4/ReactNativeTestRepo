0136ad93a11caae8c4e645ee5040abcc4af4997d
Faizan Khan <faizankh4@gmail.com>
2019-12-10 18:19:49 +0530

    commit

--- TabBarClasses/HomeDafza.js
+++ TabBarClasses/HomeDafza.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 //import react in our code.
-import { StyleSheet, View, Text ,Button,Platform,Image,Alert,FlatList,Dimensions} from 'react-native';
+import { StyleSheet, View, Text ,Button,Platform,Image,Alert,FlatList,Dimensions,ActivityIndicator,TouchableOpacity} from 'react-native';
 import SegmentedControlTab from "react-native-segmented-control-tab";
+import moment from 'moment';
 
 // import all basic components
  
 class LogoTitle extends React.Component {
     render() {
+
     //   return (
     //     <Image
     //       source={require('./../images/blueDot.png')}
@@ -86,21 +88,52 @@ makeRemoteRequest = () =>
      
       //  console.log(xml.getElementsByTagName('d:Title'));
        
-          titlearraylocal =  xml.getElementsByTagName('d:Title');
-          idarraylocal =  xml.getElementsByTagName('d:Id');
-          NewsDatearraylocal =  xml.getElementsByTagName('d:NewsDate');
-          PublishingPageContentlocal =  xml.getElementsByTagName('d:PublishingPageContent');
+        var  titlearraylocal =  xml.getElementsByTagName('d:Title');
+        var  idarraylocal =  xml.getElementsByTagName('d:Id');
+        var  NewsDatearraylocal =  xml.getElementsByTagName('d:NewsDate');
+        var  PublishingPageContentlocal =  xml.getElementsByTagName('d:PublishingPageContent');
 
-         firstdict = titlearray[0];
-         console.log(firstdict.value);
+        var reversetitleLocal = [];
+        var NewsDataArrayreverse = [];
+        var publishPagereverse = [];
+        var arraylenght = titlearraylocal.length;
+
+        for(var i = (arraylenght-1); i>=0; i--)
+         {
+              var dict = titlearraylocal[i];
+              reversetitleLocal.push(dict);
+
+         }
+        for(var i = NewsDatearraylocal.length-1 ; i>=0; i--)
+        {
+             let dict = NewsDatearraylocal[i];
+             NewsDataArrayreverse.push(dict);
+
+        
+        
+         }
+       
+       for(var i = PublishingPageContentlocal.length-1; i>=0;i--)
+       {
+       
+        publishPagereverse.push(PublishingPageContentlocal[i]);
+     
+       }
+      
+      var dict  = publishPagereverse[0];
+      console.log(dict.value);
+      
+      
+       //  firstdict = titlearray[0];
+        // console.log(firstdict.value);
         // var title = new get
        
         //  console.log(xml);
        
        
         this.setState({
-          xmlString:xml,data:xml.children,error:res.error || null, loading:false,refreshing:false,showIndicator:false,titlearray:titlearraylocal,
-          IDArray:idarraylocal,NewsDatearray:NewsDatearraylocal,PublishingPageContentArray:PublishingPageContentlocal
+          xmlString:xml,data:xml.children,error:res.error || null, loading:false,refreshing:false,showIndicator:false,titlearray:reversetitleLocal,
+          IDArray:idarraylocal,NewsDatearray:NewsDataArrayreverse,PublishingPageContentArray:PublishingPageContentlocal
         
          });
       
@@ -203,7 +236,64 @@ makeRemoteRequest = () =>
   
    }
    
+   _renderItemsecondList = (item,index) =>
+   {
+     
+    var dictNewsDate = this.state.NewsDatearray[index];
+    moment.locale('en');
+     var dt = dictNewsDate.value;
+    var formatteddate = moment(dt).format('DD-MM-YYYY');
+    
+    return (
+        <View style = {{
+            flex:1.0,
+            flexDirection:'row',
+            alignItems:'center',
+            justifyContent:'flex-start',
+           // width:'100%',
+            //width:(screenWidth-2),
+              height:50,
+          //  borderWidth:1,
+          //  margin : 5,
+           // borderRadius:10,
+            borderColor:'grey',
+            backgroundColor:'',
+            margin:1,
+       }}>
+         <Image
+           style={{height:30,width:30,margin:0}}
+           source = {require('./../images/right.png')}
+         > 
+         
+         </Image>  
+         <View style = {{flex:1,backgroundColor:'',paddingLeft:5,}}> 
+          <TouchableOpacity style = {{flex:1,justifyContent:'flex-start',alignItems:'stretch'}} >
+         <Text style = {{color:'white',fontSize:10,fontStyle:'normal',fontWeight:'500',}}
+               numberOfLines = {2}
+         >{item.value}</Text>
+         <Text style = {{color:'white',fontSize:10,fontStyle:'normal',fontWeight:'300',marginTop:3}}>{formatteddate}</Text>
+       
+       
+        </TouchableOpacity>
+         </View>  
+        
+        
+ 
+
+      
+       </View> 
+      );
+  
+  
+   }
+  
+  
+  
    render() {
+  
+     if (this.state.showIndicator)
+     {
+  
     return (
       <View style={styles.MainContainer}>
        <View style = {{height:210}}>
@@ -218,19 +308,67 @@ makeRemoteRequest = () =>
        </FlatList>  
       </View>
      
+      <View style = {{flex:1,justifyContent:'center',alignItems:'center',}}>
+       <ActivityIndicator size = 'large' color = 'white'/>
+     
+      </View>
+     
+      </View>
+     
+    
+    
+    );
+    
+  }
+ 
+  else{
+
+    return (
+      <View style={styles.MainContainer}>
+       <View style = {{height:210}}>
+       <FlatList style = {{flex:1,backgroundColor:'transparent'}} 
+                 data = {arrimage}
+                 horizontal = {true}
+                 renderItem = {({item,index}) => this._renderItem(item,index)}
+                 keyExtractor = {(item) => item.value}
+                
+       
+       >
+       </FlatList>  
+      </View>
+      <View style = {{flex:1,flexDirection:'column',backgroundColor:'rgba(15, 48, 72, 1.0)',justifyContent:'flex-start',alignItems:'stretch'}}>
+       <Text style = {{color:'white',fontWeight:'500',fontSize:16,marginLeft:5}}>Latest News</Text>
+       <FlatList style = {{backgroundColor:'',flex:1,marginTop:10}} 
+                 data = {this.state.titlearray}
+                 horizontal = {false}
+                 renderItem = {({item,index}) => this._renderItemsecondList(item,index)}
+                 keyExtractor = {(item) => item.imagekey}
+                
+       
+       >
+       </FlatList>
+     
+      
+      </View>  
+      
+      
+      
       </View>
     );
   }
+ 
+
+}
 }
  
 const styles = StyleSheet.create({
   MainContainer: {
     flex: 1,
     //paddingTop: 20,
-    alignItems: 'center',
+   // alignItems: 'center',
    // marginTop: 50,
     justifyContent:'flex-start',
-    backgroundColor:'green',
+    backgroundColor:'rgba(15, 48, 72, 1.0)',
   },tabsContainer: {
      width:65,
      marginRight:5,
