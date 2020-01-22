5d5a272c9973aa34492b1a0de2fb359f7438edf2
Faizan Khan <faizankh4@gmail.com>
2020-01-22 18:11:58 +0530

    commit

--- TabBarClasses/ReachUS/Locateus.js
+++ TabBarClasses/ReachUS/Locateus.js
@@ -7,11 +7,18 @@ import SegmentedControlTab from "react-native-segmented-control-tab";
 import renderif from './../renderif'
 //import MapView from 'react-native-maps';
 import MapView from 'react-native-maps';
+import MapViewDirections from 'react-native-maps-directions';
+import Polyline from '@mapbox/polyline';
 const Byroadtext = 'From Shaikh Zayed Road:\nOn Shaikh Zayed road, drive towards Sharjah. After crossing the Garhoud bridge, join Shaikh Rashid Road. Continue on to Dubai-Sharjah road and take exit to join Al Quds Street. DAFZA will be on your left at the third traffic signal.\n\nFrom Dubai International Airport :\nDrive on D89 with Airport on your right. Take slight right to join Shaikh Rashid Road. Continue on to Dubai-Sharjah road and take exit to join Al Quds Street. DAFZA will be on your left at the third traffic signal.'
 const ByMetroText = 'By Metro :\n\nTake the Green Line and alight at the DAFZA station. DAFZA operates a feeder bus service from DAFZA station to DAFZA '
 const phoneNo = '+971 4 299 5555'
 const screenWidth = Math.round(Dimensions.get('window').width);
 const screenHeight = Math.round(Dimensions.get('window').height);
+const GOOGLE_MAPS_APIKEY = 'AIzaSyCKj-o_mCzxqPl3WPjkZQMEUt7sXoweWGM';
+const LATITUDE = 28.5355;
+const LONGITUDE = 77.3910;
+const LATITUDE_DELTA = 25.25920633;
+const LONGITUDE_DELTA = 55.37231809;
 
 export default class LocateUS extends Component {
   //Screen2 Component
@@ -22,11 +29,49 @@ export default class LocateUS extends Component {
      selectedIndex :0,
      routeText:Byroadtext,
      showMapView:false,
+     coordinates: [
+      {
+        latitude: 28.5355,
+        longitude: 77.3910,
+      },
+      {
+        latitude: 25.25920633,
+        longitude: 5.37231809,
+      },
+    ],
+    coords: [],
   
     };
+    this.mapView = null;
+  
   }
   
-  handleIndexChange = (index) => {
+  async getDirections(startLoc, destinationLoc) {
+    try {
+        let resp = await fetch(`https://maps.googleapis.com/maps/api/directions/json?origin=${ startLoc }&destination=${ destinationLoc }&Key=GOOGLE_MAPS_APIKEY`)
+        let respJson = await resp.json();
+        let points = Polyline.decode(respJson.routes[0].overview_polyline.points);
+        let coords = points.map((point, index) => {
+            return  {
+                latitude : point[0],
+                longitude : point[1]
+            }
+        })
+        this.setState({coords: coords})
+        return coords
+    } catch(error) {
+        return error
+    }
+}
+ 
+componentDidMount()
+{
+
+  this.getDirections('28.5355, 77.3910','25.25920633, 5.37231809');
+}
+  
+
+handleIndexChange = (index) => {
   
   if (index == 0)
   {
@@ -182,11 +227,37 @@ makeCall()
        
        >
       <MapView
-         style = {{flex:1 }}
-         showsUserLocation = {true}
-         followUserLocation = {false}
-         zoomEnabled = {true}
-      />
+        //  style = {{flex:1 }}
+        //  showsUserLocation = {true}
+        //  followUserLocation = {false}
+        //  zoomEnabled = {true}
+        initialRegion={{
+          latitude: LATITUDE,
+          longitude: LONGITUDE,
+          latitudeDelta: LATITUDE_DELTA,
+          longitudeDelta: LONGITUDE_DELTA,
+        }}
+        style={{flex:1,}}
+        ref={c => this.mapView = c}
+       // onPress={this.onMapPress}
+         
+         >
+         
+          {/* <MapViewDirections
+          origin={this.state.coordinates[0]}
+           destination={this.state.coordinates[1]}
+           apikey={GOOGLE_MAPS_APIKEY}
+           strokeWidth={3}
+           strokeColor="hotpink"
+           >
+          </MapViewDirections> */}
+           <MapView.Polyline 
+            coordinates={this.state.coords}
+            strokeWidth={2}
+            strokeColor="red"/>
+         
+         </MapView>
+      
      
      <TouchableOpacity style = {{width:40,height:40,position:'absolute'}}
                           onPress = {this.removeWebView.bind(this)} 
